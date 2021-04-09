c1bf7f9e6c50bf9c81122396457cee696685e10a
Nick Gaudet <59885900+Nick-Gaudet@users.noreply.github.com>
2021-04-09 00:50:25 -0400

    Changed background colour to green

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,20 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+
+import java.io.DataInputStream;
+import java.net.Socket;
 
 // Client
 
 public class Main extends Application {
 
     //FIELDS
+    final private int PORT = 1234;
     final private int SCREEN_WIDTH = 1024;
     final private int SCREEN_HEIGHT = 768;
 
@@ -20,25 +26,29 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         Group group = new Group();
+        Pane pane  = new Pane();
+        StackPane background = new StackPane();
         Canvas canvas = new Canvas(SCREEN_WIDTH, SCREEN_HEIGHT);
         GraphicsContext gc = canvas.getGraphicsContext2D();
-        group.getChildren().add(canvas);
-
+        background.getChildren().add(canvas);
+        group.getChildren().add(background);
+        background.setStyle("-fx-background-color: green");
         // TODO: thread for draw
         // TODO: socket stuff
 
         primaryStage.setTitle("Othello");
         primaryStage.setScene(new Scene(group));
         primaryStage.show();
-        draw(gc);
+        drawInitialBoardState(gc);
     }
 
     public static void main(String[] args) {
         launch(args);
     }
 
-    public void draw(GraphicsContext gc) {
+    public void drawInitialBoardState(GraphicsContext gc) {
         // Board drawn to initial state
+
         gc.setFill(Color.BLACK);
         for(int x = 0 ; x <= SCREEN_WIDTH; x++){
             for (int y = 0 ; y <= SCREEN_HEIGHT; y++){
