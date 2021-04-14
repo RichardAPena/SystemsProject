7057d45fb861607f392821b399a12674f701a414
Nick Gaudet <59885900+Nick-Gaudet@users.noreply.github.com>
2021-04-14 00:22:38 -0400

    waits for two players to connect

--- src/Server.java
+++ src/Server.java
@@ -15,28 +15,39 @@ public class Server {
         Socket s1 = ss.accept();
         Player p1 = new Player(s1);
         System.out.println("Player 1 found");
+        Socket s2 = ss.accept();
+        Player p2 = new Player(s2);
+        System.out.println("Player 2 found");
 
+//        sendBoardData(board,s2);
+        System.out.println("Created board");
+//        System.out.println("Game done");
         while(true){ // temporary, wanted to see if board would update based on client
 //            sendBoardData(board,s1);
-            ObjectOutputStream output = new ObjectOutputStream(s1.getOutputStream());
-            DataInputStream in = new DataInputStream(s1.getInputStream());
-//            DataOutputStream out = new DataOutputStream(s1.getOutputStream());
-            int x = Integer.parseInt(in.readUTF().split(" ")[0]);
-            int y = Integer.parseInt(in.readUTF().split(" ")[1]);
-            System.out.println("x : " + x + " y : " + y);
-            board.getBoard()[x][y] = "X";
-            output.writeObject(board.getBoard());
-            output.flush();
+            ObjectOutputStream outputPlayer1 = new ObjectOutputStream(s1.getOutputStream());
+            DataInputStream inputPlayer1 = new DataInputStream(s1.getInputStream());
+            ObjectOutputStream outputPlayer2 = new ObjectOutputStream(s2.getOutputStream());
+            DataInputStream inputPlayer2 = new DataInputStream(s2.getInputStream());
+
+            int xP1 = Integer.parseInt(inputPlayer1.readUTF().split(" ")[0]);
+            int yP1 = Integer.parseInt(inputPlayer1.readUTF().split(" ")[1]);
+            int xP2 = Integer.parseInt(inputPlayer2.readUTF().split(" ")[0]);
+            int yP2 = Integer.parseInt(inputPlayer2.readUTF().split(" ")[1]);
+
+            System.out.println("xP1 : " + xP1 + " yP1 : " + yP1);
+            System.out.println("xP2 : " + xP2 + " yP2 : " + yP2);
+
+            board.getBoard()[xP1][yP1] = "X";
+            board.getBoard()[xP2][yP2] = "Y";
+
+            outputPlayer1.writeObject(board.getBoard());
+            outputPlayer1.flush();
+            outputPlayer2.writeObject(board.getBoard());
+            outputPlayer2.flush();
             System.out.println(board);
 
         }
-//        Socket s2 = ss.accept();
-//        Player p2 = new Player(s2);
-//        System.out.println("Player 2 found");
-//
-//        sendBoardData(board,s2);
-//        System.out.println("Created board");
-//        System.out.println("Game done");
+
     }
     public static void sendBoardData(Board board, Socket p){
         try{
