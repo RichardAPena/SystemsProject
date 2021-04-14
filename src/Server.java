48bfe8aa13b71c6382e902c83c178ac28879e219
Nick Gaudet <59885900+Nick-Gaudet@users.noreply.github.com>
2021-04-14 01:24:55 -0400

    trying threading at server end

--- src/Server.java
+++ src/Server.java
@@ -29,26 +29,38 @@ public class Server {
 //        System.out.println("Game done");
         while(true){ // temporary, wanted to see if board would update based on client
 //            sendBoardData(board,s1);
+
             ObjectOutputStream outputPlayer1 = new ObjectOutputStream(s1.getOutputStream());
             DataInputStream inputPlayer1 = new DataInputStream(s1.getInputStream());
             ObjectOutputStream outputPlayer2 = new ObjectOutputStream(s2.getOutputStream());
             DataInputStream inputPlayer2 = new DataInputStream(s2.getInputStream());
+            new Thread(() ->{
+                try {
+                    int xP1 = Integer.parseInt(inputPlayer1.readUTF().split(" ")[0]);
+                    int yP1 = Integer.parseInt(inputPlayer1.readUTF().split(" ")[1]);
+                    System.out.println("xP1 : " + xP1 + " yP1 : " + yP1);
+                    board.getBoard()[xP1][yP1] = "X";
+                    outputPlayer1.writeObject(board.getBoard());
+                    outputPlayer1.flush();
 
-            int xP1 = Integer.parseInt(inputPlayer1.readUTF().split(" ")[0]);
-            int yP1 = Integer.parseInt(inputPlayer1.readUTF().split(" ")[1]);
-            int xP2 = Integer.parseInt(inputPlayer2.readUTF().split(" ")[0]);
-            int yP2 = Integer.parseInt(inputPlayer2.readUTF().split(" ")[1]);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }).start();
+            new Thread(() ->{
+                try {
+                    int xP2 = Integer.parseInt(inputPlayer2.readUTF().split(" ")[0]);
+                    int yP2 = Integer.parseInt(inputPlayer2.readUTF().split(" ")[1]);
+                    System.out.println("xP2 : " + xP2 + " yP2 : " + yP2);
+                    board.getBoard()[xP2][yP2] = "Y";
+                    outputPlayer2.writeObject(board.getBoard());
+                    outputPlayer2.flush();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
 
-            System.out.println("xP1 : " + xP1 + " yP1 : " + yP1);
-            System.out.println("xP2 : " + xP2 + " yP2 : " + yP2);
+            }).start();
 
-            board.getBoard()[xP1][yP1] = "X";
-            board.getBoard()[xP2][yP2] = "Y";
-
-            outputPlayer1.writeObject(board.getBoard());
-            outputPlayer1.flush();
-            outputPlayer2.writeObject(board.getBoard());
-            outputPlayer2.flush();
             System.out.println(board);
 
         }
