c3a8df34b17e18204e041c96b38f2b152d00c804
RichardAPena <richardalexpena@hotmail.com>
2021-04-14 12:12:05 -0400

    experimental changes to server and player

--- src/Player.java
+++ src/Player.java
@@ -8,13 +8,39 @@ public class Player extends Thread {
 
     private BufferedReader in;
     private PrintWriter out;
+    private Board board;
+    Player opponnent;
+    private boolean goNext;
+    private String piece;
 
-    public Player(Socket s) throws IOException {
-        in = new BufferedReader(new InputStreamReader(s.getInputStream()));
-        out = new PrintWriter(s.getOutputStream());
+    public Player(Socket player, Board board, String piece) throws IOException {
+        in = new BufferedReader(new InputStreamReader(player.getInputStream()));
+        out = new PrintWriter(player.getOutputStream());
+        this.board = board;
+        //this.opponnent = opponent;
+        this.piece = piece;
+        goNext = piece.equals("X");
     }
 
     public void run() {
         // TODO
+        String request = "";
+        // Listen to client requests
+        while (true) {
+            try {
+                if (goNext) {
+                    out.println("YOURTURN");
+                    goNext = false;
+                }
+                request = in.readLine();
+                if (request.startsWith("MAKEMOVE")) {
+
+                } else if (request.startsWith("PASS")) {
+
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
